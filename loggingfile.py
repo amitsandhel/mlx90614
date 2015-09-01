c8f77c72935c543623f08976eb168b264b85e4e6
amitsandhel <amit.sandhel@gmail.com>
2015-09-01 10:17:22 -0400

    first comiit
    with '#' will be ignored, and an empty message aborts the commit.

---
+++ loggingfile.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#loggingfile.py
+
+import sys
+import time
+import logging
+import glob
+import logging.handlers
+'''Logging class that handles the logging of all files 
+'''
+
+#https://docs.python.org/2/howto/logging-cookbook.html#using-file-rotation
+#https://docs.python.org/2/library/logging.handlers.html#rotatingfilehandler
+
+    
+class Logging_File():
+    def __init__(self, logger_name, level=logging.DEBUG): # log_file, level=logging.DEBUG):
+        #self.log_file = log_file
+        self.level = level
+        self.name = logger_name
+        self.l = logging.getLogger(logger_name)
+        self.setup_logger()
+        
+    def setup_logger(self): #, logger_name, log_file, level=logging.DEBUG):
+        """This function is designed for logging setup"""
+        formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
+        
+        #fileHandler = logging.FileHandler(self.log_file, mode='a')
+        #fileHandler.setFormatter(formatter)
+        
+        rotateHandler = logging.handlers.RotatingFileHandler(self.name, mode='a', maxBytes=0, backupCount=1, delay=0) # backupCount=2, encoding=None, delay=0)
+        rotateHandler.setFormatter(formatter)
+        
+        streamHandler = logging.StreamHandler()
+        streamHandler.setFormatter(formatter)
+        
+        
+        self.l.setLevel(self.level)
+        #self.l.addHandler(fileHandler)
+        self.l.addHandler(rotateHandler)
+        
+        #rotateHandler.doRollover()
+        
+        
+        #uncommenting this line below allows you to print all the logs to the dos terminal
+        #self.l.addHandler(streamHandler)  
+
+
+
